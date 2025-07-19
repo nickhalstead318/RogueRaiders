2b270c550fde6847f0fd02dfa5a8af19428da2fc
JSchade44 <jschade@udel.edu>
2025-07-19 15:12:53 -0500

    Add Card Behavior
    Added functionality for the card to be dragged, hovered, and released

---
+++ Rogue Raiders/Assets/Scripts/CardBehavior.cs
@@ -0,0 +1,88 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CardBehavior : MonoBehaviour
+{
+    public Color hoverColor = Color.red;
+    public float cardSpeed = 10.0f;
+
+    private Renderer objectRenderer;
+    private Vector3 hoveredPosition;
+    private Vector3 stationaryPosition;
+    private CardDirection direction;
+    private bool isDragging = false;
+
+    public enum CardDirection
+    {
+        Hovered,
+        Unhovered,
+        Stationary,
+    }
+
+    void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();
+        direction = CardDirection.Stationary;
+        stationaryPosition = new Vector3(transform.position.x, -35, transform.position.z);
+        hoveredPosition = new Vector3(transform.position.x, -18, transform.position.z);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1) && isDragging)
+        {
+            ReleaseCard();
+        }
+
+        if (isDragging)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = Camera.main.WorldToScreenPoint(transform.position).z; // Maintain Z distance
+            Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = worldMousePosition;
+        }
+        else if (direction == CardDirection.Hovered || direction == CardDirection.Unhovered)
+        {
+            Vector3 destination = direction == CardDirection.Hovered ? hoveredPosition : stationaryPosition;
+            transform.position = Vector3.MoveTowards(transform.position, destination, cardSpeed*Time.deltaTime);
+            if (transform.position.Equals(destination))
+            {
+                direction = CardDirection.Stationary;
+            }
+        }
+    }
+
+    public void OnMouseEnter()
+    {
+        // Highlight the object
+        objectRenderer.material.color = hoverColor;
+        direction = CardDirection.Hovered;
+    }
+
+    public void OnMouseExit()
+    {
+        objectRenderer.material.color = Color.white;
+        direction = CardDirection.Unhovered;
+    }
+
+    public void OnMouseDown()
+    {
+        if (!isDragging)
+        {
+            isDragging = true;
+        }
+        else
+        {
+            ReleaseCard();
+        }
+    }
+
+    private void ReleaseCard()
+    {
+        isDragging = false;
+        if (!transform.position.Equals(hoveredPosition))
+        {
+            direction = CardDirection.Hovered;
+        }
+    }
+}

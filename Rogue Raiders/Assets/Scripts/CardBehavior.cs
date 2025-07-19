596ccbc3341d652c930a2a43bc2997d8ff79261e
nickhalstead318 <nickhalstead2001@yahoo.com>
2025-07-19 18:54:27 -0500

    Fix and enhance card behavior
    This commit fixes some merge issues combining the card object and map into the same scene. Additionally, this commit adds some minor visual enhancements.

--- Rogue Raiders/Assets/Scripts/CardBehavior.cs
+++ Rogue Raiders/Assets/Scripts/CardBehavior.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -5,10 +6,11 @@ public class CardBehavior : MonoBehaviour
 {
     public Color hoverColor = Color.red;
     public float cardSpeed = 10.0f;
+    [SerializeField] private int cardIndex = 0;
+    [SerializeField] Vector3 stationaryPos = new Vector3(65, 5, 0);
+    [SerializeField] Vector3 hoveredPos = new Vector3(40, 5, 0);
 
     private Renderer objectRenderer;
-    private Vector3 hoveredPosition;
-    private Vector3 stationaryPosition;
     private CardDirection direction;
     private bool isDragging = false;
     private GameManager gameManager;
@@ -26,36 +28,47 @@ public class CardBehavior : MonoBehaviour
         objectRenderer = GetComponent<Renderer>();
         direction = CardDirection.Stationary;
 
-        // These are the positions based on a main camera at 0, 50, 0 and at angle 90, -90, 0
-        stationaryPosition = new Vector3(50, -39, 0);
-        transform.position = stationaryPosition;
-        hoveredPosition = new Vector3(33, transform.position.y, transform.position.z);
+        // 
+        hoveredPos.z = getZ();
+        stationaryPos.z = getZ();
+
+        transform.position = stationaryPos;
+    }
+
+    private float getZ()
+    {
+        return (transform.localScale.z * (cardIndex - 2)) + Camera.main.transform.position.z;
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(1) && isDragging)
-        {
-            ReleaseCard();
-        }
-
         if (isDragging)
         {
+            if (Input.GetMouseButtonDown(1))
+            {
+                ReleaseCard();
+            }
+
             Vector3 mousePosition = Input.mousePosition;
             mousePosition.z = Camera.main.WorldToScreenPoint(transform.position).z; // Maintain Z distance
-            Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            transform.position = worldMousePosition;
+            transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+            return; // Early quits if dragging
         }
-        else if (direction == CardDirection.Hovered || direction == CardDirection.Unhovered)
+
+        if (direction == CardDirection.Hovered || direction == CardDirection.Unhovered)
         {
-            Vector3 destination = direction == CardDirection.Hovered ? hoveredPosition : stationaryPosition;
-            transform.position = Vector3.MoveTowards(transform.position, destination, cardSpeed*Time.deltaTime);
-            if (transform.position.Equals(destination))
+            Vector3 destination = (direction == CardDirection.Hovered) ? hoveredPos : stationaryPos;
+            transform.position = Vector3.MoveTowards(transform.position, destination, cardSpeed * Time.deltaTime);
+
+            // Stationary in threshhold
+            if (Vector3.Distance(transform.position, destination) < 0.01f)
             {
+                transform.position = destination; // Snaps card
                 direction = CardDirection.Stationary;
             }
         }
     }
+
 
     public void OnMouseEnter()
     {
@@ -66,6 +79,7 @@ public class CardBehavior : MonoBehaviour
 
     public void OnMouseExit()
     {
+        if (isDragging) return; // Don't unhover if actively dragging
         objectRenderer.material.color = Color.white;
         direction = CardDirection.Unhovered;
     }
@@ -85,7 +99,7 @@ public class CardBehavior : MonoBehaviour
     private void ReleaseCard()
     {
         isDragging = false;
-        if (!transform.position.Equals(hoveredPosition))
+        if (!transform.position.Equals(hoveredPos))
         {
             direction = CardDirection.Hovered;
         }
